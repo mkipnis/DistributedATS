35ca835ed9595349dc7c288fa9d06eca8b2a37e5
Mike Kipnis <mike.kipnis@gmail.com>
2023-02-11 16:09:04 +0000

    updated explicit index.html mapping for tomcat

--- MiscClients/spring_reactjs/src/main/java/org/DistributedATS/WebTraderRest/WebConfig.java
+++ MiscClients/spring_reactjs/src/main/java/org/DistributedATS/WebTraderRest/WebConfig.java
@@ -29,8 +29,12 @@
 package org.DistributedATS.WebTraderRest;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import java.io.IOException;
 
 @Configuration
 public class WebConfig  implements WebMvcConfigurer {
@@ -42,8 +46,18 @@ private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-            .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+	     registry.addResourceHandler("/*")
+        .addResourceLocations("classpath:/static/")
+        .resourceChain(true)
+        .addResolver(new PathResourceResolver() {
+            @Override
+            protected Resource getResource(String resourcePath,
+                Resource location) throws IOException {
+                Resource requestedResource = location.createRelative(resourcePath);
+                return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                : new ClassPathResource("/static/index.html");
+            }
+        });
 	}
-    
+
 }
