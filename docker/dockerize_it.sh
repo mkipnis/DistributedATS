5128a44855af9a2ffa8551586d22654a970124c5
Mike Kipnis <mike.kipnis@gmail.com>
2023-04-23 15:30:43 -0400

    dockerized

--- docker/dockerize_it.sh
+++ docker/dockerize_it.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+set -x 
+
 # Core
 #docker build -t distributed_ats -f Docker.build_distributed_ats .
 docker build -t ghcr.io/mkipnis/distributed_ats:latest -f Docker.build_distributed_ats .
 docker push ghcr.io/mkipnis/distributed_ats:latest
 
 # Front-end
+DOCKER_DIR=`pwd`
+ln -sf ../MiscClients/spring_reactjs/ WebTrader
+cp fixclient_docker.cfg WebTrader/src/main/resources/fixclient.cfg
+cd WebTrader
+mvn clean
+mvn install
+cp target/WebTraderRest-0.0.1-SNAPSHOT.war $DOCKER_DIR
+cd $DOCKER_DIR
+
 #docker build -t distributed_ats_webtrader -f Docker.WebTrader .
 docker build -t ghcr.io/mkipnis/distributed_ats_webtrader:latest -f Docker.WebTrader .
 docker push ghcr.io/mkipnis/distributed_ats_webtrader:latest
