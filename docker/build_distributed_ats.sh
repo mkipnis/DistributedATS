3832864475a41c32128585b461005da3bd0f5474
Mike Kipnis <mike.kipnis@gmail.com>
2025-05-06 21:13:17 +0000

    updated docker

--- docker/build_distributed_ats.sh
+++ docker/build_distributed_ats.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-[[ ! -f /opt/distributed_ats_src ]] && git clone -b FastDDS https://github.com/mkipnis/DistributedATS /opt/distributed_ats_src
+[[ ! -f /opt/distributed_ats_src ]] && git clone -b master https://github.com/mkipnis/DistributedATS /opt/distributed_ats_src
 
 cd /opt/distributed_ats_src
-rm -rf build
-mkdir build
-cd build
-
-export DEPS_INSTALL_DIR=/opt/distributed_ats_deps_build/
-cmake .. -Dfastcdr_DIR=$DEPS_INSTALL_DIR/lib/cmake/fastcdr/ -Dfastrtps_DIR=$DEPS_INSTALL_DIR/share/fastrtps/cmake/ -Dfoonathan_memory_DIR=$DEPS_INSTALL_DIR/lib/foonathan_memory/cmake/ -Dlog4cxx_DIR=$DEPS_INSTALL_DIR/lib/cmake/log4cxx -DCMAKE_INSTALL_PREFIX=/opt/DistributedATS -DLIQUIBOOK_HOME=$DEPS_INSTALL_DIR/liquibook/src -DQUICKFIX_INSTALL_PREFIX=$DEPS_INSTALL_DIR
-
-cmake --build . --target install --config Debug
-
-chmod +x /opt/DistributedATS/scripts/*.sh
-chmod +x /opt/DistributedATS/MiscATS/MultiMatchingEngineATS/scripts/*.sh
-chmod +x /opt/DistributedATS/MiscATS/USTreasuryCLOB/scripts/*.sh
-chmod +x /opt/DistributedATS/MiscATS/CryptoCLOB/scripts/*.sh
+HOME=/opt ./download_deps_and_build_all.sh
