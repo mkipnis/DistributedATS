4f757c77413526b0f30850ae76d4c1e24a00c175
Mike Kipnis <mike.kipnis@gmail.com>
2025-05-18 18:12:31 -0400

    cmake update

--- docker/build_distributed_ats.sh
+++ docker/build_distributed_ats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ ! -f /opt/distributed_ats_src ]] && git clone -b master https://github.com/mkipnis/DistributedATS /opt/distributed_ats_src
+[[ ! -f /opt/distributed_ats_src ]] && git enhancements_0424 -b master https://github.com/mkipnis/DistributedATS /opt/distributed_ats_src
 
 cd /opt/distributed_ats_src
-HOME=/opt ./download_deps_and_build_all.sh
+HOME=/opt ./build_with_cmake.sh
