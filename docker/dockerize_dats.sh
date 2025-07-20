2a642d452575556db29e29097296099a1f26484e
Mike Kipnis <mike.kipnis@gmail.com>
2025-07-20 19:56:25 +0000

    docker updated

--- docker/dockerize_dats.sh
+++ docker/dockerize_dats.sh
@@ -3,10 +3,12 @@
 set -x 
 
 # Core
-docker build --no-cache -t ghcr.io/mkipnis/distributed_ats:latest -f Docker.Build_Distributed_ATS .
+docker build -t ghcr.io/mkipnis/distributed_ats_deps:latest -f Docker.Build_DATS_Deps .
+docker build -t ghcr.io/mkipnis/distributed_ats:latest -f Docker.Build_Distributed_ATS .
 docker build --no-cache -t ghcr.io/mkipnis/dats_crypto_clob:latest -f Docker.Crypto_CLOB .
 docker build --no-cache -t ghcr.io/mkipnis/dats_ust_clob:latest -f Docker.UST_CLOB .
 
-#docker push ghcr.io/mkipnis/distributed_ats:latest
-#docker push ghcr.io/mkipnis/dats_crypto_clob:latest
-#docker push ghcr.io/mkipnis/dats_ust_clob:latest
+docker push ghcr.io/mkipnis/distributed_ats_deps:latest
+docker push ghcr.io/mkipnis/distributed_ats:latest
+docker push ghcr.io/mkipnis/dats_crypto_clob:latest
+docker push ghcr.io/mkipnis/dats_ust_clob:latest
