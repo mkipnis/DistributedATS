daa54e4e1d0ef8d274907cdb87849a45d79a386c
Ian Ngulube <ngulubeian94@gmail.com>
2026-03-10 10:04:49 +0200

    chore:initial customization

--- docker/build_distributed_ats.sh
+++ docker/build_distributed_ats.sh
@@ -3,5 +3,5 @@
 git clone -b master https://github.com/ianngulube/DistributedATS /opt/distributed_ats_src
 
 cd /opt/distributed_ats_src
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX=/usr/local -DDDS_ROOT_DIR=/usr/local -DLOG4CXX_ROOT_DIR=/usr/local -DQUICKFIX_ROOT_DIR=/usr/local -DLIQUIBOOK_ROOT_DIR=/usr/local
+cmake -S . -B build -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=/usr/local -DDDS_ROOT_DIR=/usr/local -DLOG4CXX_ROOT_DIR=/usr/local -DQUICKFIX_ROOT_DIR=/usr/local -DLIQUIBOOK_ROOT_DIR=/usr/local
 cmake --build build --verbose --target install
